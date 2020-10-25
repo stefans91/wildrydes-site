50ea6c43e61ad83136d23d7deb3ba8895e9000af
stefans91 <S.Schramedei@gmail.com>
2020-10-25 22:37:54 +0100

    userpool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_4BwsN1mjY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6rka99h75p04tpp6c1tab9o46j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
